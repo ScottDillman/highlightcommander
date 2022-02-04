6ba1c16804947e4b74a2b2b56daaa2e506e5adb9
Scott Dillman <scott@bitwise.ninja>
2022-02-04 17:21:17 -0500

    windows fixes

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,3 +1,12 @@
+#ifdef  _WIN32
+
+#define WIN32_LEAN_AND_MEAN      // Exclude rarely-used stuff from Windows headers
+
+#include <windows.h>
+
+#endif
+
+
 #include <QFile>
 #include <QFontDatabase>
 #include <QCoreApplication>
